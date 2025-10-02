0e23fac04591de1d70fb36c5e2567046d575e413
eric-on-orin06 <ericacccls@gmail.com>
2025-10-02 18:07:51 +0800

    trying to use USB to communicate between Arduino and Jetson Orin Nano. Connect Arduino to TMC2209

--- test/tmc2209-with-arduino-uno/arduino-uno-serial-monitor/send_message_to_uart.py
+++ test/tmc2209-with-arduino-uno/arduino-uno-serial-monitor/send_message_to_uart.py
@@ -3,7 +3,7 @@ import time
 
 # Configure the UART serial port
 ser = serial.Serial(
-    port="/dev/ttyTHS1",    # UART1 on Jetson Orin Nano
+    port="/dev/ttyACM0",    # UART1 on Jetson Orin Nano
     baudrate=115200,          # Match Arduino's baudrate
     timeout=1
 )
@@ -16,6 +16,10 @@ try:
         ser.write(message.encode('ascii'))
         print(f"📤 Sent: {message.strip()}")
         count += 1
+
+        while ser.in_waiting > 0:
+            print(ser.read().decode('ascii'), end='', flush=True)
+
         time.sleep(1)  # send every 1 second
 
 except KeyboardInterrupt:
