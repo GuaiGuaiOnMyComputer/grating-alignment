a6560cdb588672186c1ddd6ac2ac6c2f36ce5884
eric-on-orin06 <ericacccls@gmail.com>
2025-10-03 09:36:44 +0800

    updated pin configuration

--- dep/arduinounostepper_TMC2209/pinconfig.h
+++ dep/arduinounostepper_TMC2209/pinconfig.h
@@ -1,4 +1,4 @@
-#define ORIN_NANO_BAUD_RATE 115200
-#define ORIN_NANO_RX_PIN    10
-#define ORIN_NANO_TX_PIN    11
-#define ENABLE_PIN          4
+#define UNO_BAUD_RATE 115200
+#define UNO_RX_PIN    10
+#define UNO_TX_PIN    11
+#define ENABLE_PIN    4
